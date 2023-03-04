d5753c447e66ba086bceebd30b798449be680d9e
Alex052524 <abdul_akhmedov_2004@mail.ru>
2023-03-04 15:27:57 +0300

    во getAllUsers

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -7,10 +7,8 @@ import java.sql.*;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
-        Statement statement = Util.getStatement();
-//        statement.execute("INSERT INTO Users(name, lastName, age) VALUES('Alex', 'Sidorov', 123 );");
         UserServiceImpl service = new UserServiceImpl();
-        service.createUsersTable();
+
         Util.close();
     }
 }
--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -5,6 +5,7 @@ import jm.task.core.jdbc.util.Util;
 
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Arrays;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
@@ -15,7 +16,7 @@ public class UserDaoJDBCImpl implements UserDao {
 
     public void createUsersTable() {
         try {
-            statement.execute("CREATE TABLE IF NOT EXIST `users` (\n" +
+            statement.execute("CREATE TABLE IF NOT EXISTS `users` (\n" +
                     "  `idUsers` INT NOT NULL AUTO_INCREMENT,\n" +
                     "  `name` VARCHAR(45) NOT NULL,\n" +
                     "  `lastName` VARCHAR(45) NOT NULL,\n" +
@@ -27,15 +28,27 @@ public class UserDaoJDBCImpl implements UserDao {
     }
 
     public void dropUsersTable() {
-
+        try {
+            statement.execute("DROP TABLE IF EXISTS `users`");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public void saveUser(String name, String lastName, byte age) {
-
+        try {
+            statement.execute(String.format("INSERT INTO Users(name, lastName, age) VALUES('%s', '%s', %d );", name, lastName, age));
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public void removeUserById(long id) {
-
+        try {
+            statement.execute(String.format("DELETE FROM `Users` WHERE `idUsers` = %d", id));
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public List<User> getAllUsers() {
@@ -43,6 +56,10 @@ public class UserDaoJDBCImpl implements UserDao {
     }
 
     public void cleanUsersTable() {
-
+        try {
+            statement.execute("DELETE FROM users;");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
