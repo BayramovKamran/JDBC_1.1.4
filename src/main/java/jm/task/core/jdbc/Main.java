5408fd3a64094984c78ddf78cac841adc0a6c3d8
Alex052524 <abdul_akhmedov_2004@mail.ru>
2023-03-04 10:56:14 +0300

    firts

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,38 @@
 package jm.task.core.jdbc;
 
+import java.sql.*;
+
 public class Main {
-    public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+    public static void main(String[] args) throws SQLException {
+        ResultSet resultSet = DataBase.statement.executeQuery("SELECT * FROM pp1.users");
+        while (resultSet.next()) {
+            System.out.println(resultSet.getString(2) + " " +
+                    resultSet.getString(3) + " " +
+                    resultSet.getString(4));
+        }
+        DataBase.connection.close();
+        DataBase.statement.close();
+    }
+}
+class DataBase {
+    public static final String URL = "jdbc:mysql://127.0.0.1:3306/";
+    public static final String USER = "root";
+    public static final String PASSWORD = "12345678";
+    public static Connection connection;
+    public static Statement statement;
+
+    static {
+        try {
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    static {
+        try {
+            statement = connection.createStatement();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
