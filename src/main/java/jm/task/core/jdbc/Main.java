71967a7c4540acc381e39bb4058df430bfeaeb2b
Alex052524 <abdul_akhmedov_2004@mail.ru>
2023-03-08 00:58:39 +0300

    delete close

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,12 +1,9 @@
 package jm.task.core.jdbc;
 
 import jm.task.core.jdbc.service.UserServiceImpl;
-import jm.task.core.jdbc.util.Util;
-
-import java.sql.*;
 
 public class Main {
-    public static void main(String[] args) throws SQLException {
+    public static void main(String[] args) {
         UserServiceImpl service = new UserServiceImpl();
         service.createUsersTable();
         service.saveUser("Миша", "Черешков", (byte) 123);
@@ -16,6 +13,5 @@ public class Main {
         System.out.println(service.getAllUsers());
         service.cleanUsersTable();
         service.dropUsersTable();
-        Util.close();
     }
 }
